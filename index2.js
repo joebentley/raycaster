37de636af74418c3b323872052000d7e5a6520cd
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2018-02-15 15:44:21 +0000

    Successfully refactored engine

--- index2.js
+++ index2.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function Ray(origin, angle) {
     this.origin = origin
     this.angle = angle
@@ -10,7 +12,7 @@ Ray.prototype.cast = function (collisionFunc) {
         const rayY = this.origin.y + distance * Math.sin(Math.PI / 180 * this.angle)
 
         let collisionResult = collisionFunc(rayX, rayY, distance)
-        if (collisionResult.hit === true) {
+        if (collisionResult !== null) {
             return collisionResult
         }
     }
@@ -50,17 +52,13 @@ RayCaster.prototype.cast = function (origin, facing, collisionFunc) {
     return zArray
 }
 
-function World() {
+function GridWorld(grid, player) {
+    this.grid = grid
+    this.player = player
 }
-
-function GridWorld() {
-    this.grid = [[]]
-}
-
-GridWorld.prototype = World
 
 GridWorld.prototype.collisionFunc = function (perspectiveAngle) {
-    return function (x, y, distance) {
+    return (x, y, distance) => {
         const squareType = this.grid[Math.floor(y)][Math.floor(x)]
         if (squareType != 0) {
             return {
@@ -72,12 +70,15 @@ GridWorld.prototype.collisionFunc = function (perspectiveAngle) {
     }
 }
 
-GridWorld.prototype.render = function (ctx) {
+GridWorld.prototype.render = function (width, height, ctx, rayCaster) {
     var imageData = ctx.createImageData(width, height)
     var data = imageData.data
 
+    const zArray = rayCaster.cast(this.player.position, this.player.facing,
+        this.collisionFunc.bind(this))
+
     for (let x = 0; x < width; ++x) {
-        const columnIndex = Math.floor(x / columnWidth)
+        const columnIndex = Math.floor(x / rayCaster.columnWidth)
         const wallHeight = height / zArray[columnIndex].z
         const shadingFactor = Math.min(1, 1.4/zArray[columnIndex].z + 0.3)
 
@@ -108,17 +109,94 @@ GridWorld.prototype.render = function (ctx) {
     ctx.putImageData(imageData, 0, 0)
 }
 
-function Renderer(ctx, rayCaster) {
-    this.ctx = ctx
-    this.rayCaster = rayCaster
-}
+// function Renderer(ctx, rayCaster) {
+//     this.ctx = ctx
+//     this.rayCaster = rayCaster
+// }
+//
+// Renderer.prototype.render(origin, facing, world) {
+//     this.rayCaster.cast(origin, facing, world.collisionFunc)
+// }
 
-Renderer.prototype.render(origin, facing, world) {
-    this.rayCaster.cast(origin, facing, world.collisionFunc)
-}
-
-function Player(x, y, facing) {
-    this.x = x
-    this.y = y
+function Player(position, facing) {
+    this.position = position
     this.facing = facing
 }
+
+function App() {
+    this.canvas = document.getElementById('canvas')
+    this.ctx = this.canvas.getContext('2d')
+    this.width = this.canvas.width
+    this.height = this.canvas.height
+    this.rayCaster = new RayCaster(60, 1, 1, this.width)
+
+    const roomData = [
+        [1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,1],
+        [1,0,0,2,0,0,0,1],
+        [1,0,0,2,0,0,0,1],
+        [1,0,0,2,0,0,0,1],
+        [1,0,0,2,0,0,0,1],
+        [1,1,1,1,1,1,0,1],
+        [1,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1]
+    ]
+
+    this.player = new Player({x: 5, y: 4}, 0)
+    this.gridWorld = new GridWorld(roomData, this.player)
+
+    this.keys = {}
+
+    window.addEventListener('keydown', (e) => {
+        this.keys[e.keyCode] = true;
+    })
+    window.addEventListener('keyup', (e) => {
+        this.keys[e.keyCode] = false;
+    })
+}
+
+App.prototype.run = function () {
+    window.requestAnimationFrame(this.loop.bind(this))
+}
+
+App.prototype.loop = function () {
+    if (this.keys[37]) {
+        this.player.facing -= 4;
+    }
+    if (this.keys[39]) {
+        this.player.facing += 4;
+    }
+    if (this.keys[38]) {
+        const newX = this.player.position.x + .1 * Math.cos(Math.PI / 180 * this.player.facing)
+        const newY = this.player.position.y + .1 * Math.sin(Math.PI / 180 * this.player.facing)
+        // Don't let player get too close to wall
+        const collX = this.player.position.x + .4 * Math.cos(Math.PI / 180 * this.player.facing)
+        const collY = this.player.position.y + .4 * Math.sin(Math.PI / 180 * this.player.facing)
+        if (this.gridWorld.collisionFunc(0)(collX, this.player.position.y, 0) === null) {
+            this.player.position.x = newX
+        }
+        if (this.gridWorld.collisionFunc(0)(this.player.position.x, collY, 0) === null) {
+            this.player.position.y = newY
+        }
+    }
+    if (this.keys[40]) {
+        const newX = this.player.position.x - .1 * Math.cos(Math.PI / 180 * this.player.facing)
+        const newY = this.player.position.y - .1 * Math.sin(Math.PI / 180 * this.player.facing)
+        // Don't let player get too close to wall
+        const collX = this.player.position.x - .4 * Math.cos(Math.PI / 180 * this.player.facing)
+        const collY = this.player.position.y - .4 * Math.sin(Math.PI / 180 * this.player.facing)
+        if (this.gridWorld.collisionFunc(0)(collX, this.player.position.y, 0) === null) {
+            this.player.position.x = newX
+        }
+        if (this.gridWorld.collisionFunc(0)(this.player.position.x, collY, 0) === null) {
+            this.player.position.y = newY
+        }
+    }
+
+    this.gridWorld.render(this.width, this.height, this.ctx, this.rayCaster)
+    window.requestAnimationFrame(this.loop.bind(this))
+}
+
+new App().run()
