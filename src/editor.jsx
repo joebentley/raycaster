669a58e807a544ef17015bd59536a6c11bdcf5f6
Joe Bentley <joebentley10@gmail.com>
2018-02-21 17:10:27 +0000

    Change to functional component

--- src/editor.jsx
+++ src/editor.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Square(onClick) {
-  return <button className="square" onClick={onClick} />;
+function Square(props) { // eslint-disable-line
+  return <button className="square" onClick={props.onClick} />;
 }
 
 export default class Editor extends React.Component {
   render () {
-    return <div className="editor">{Square(function () { console.log('hello'); })}</div>
+    return <div className="editor"><Square onClick={() => { console.log('hello');}} /></div>;
   }
 }
